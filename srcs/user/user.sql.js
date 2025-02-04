b7ddbb1d2c6958ad179e337fc28bab5bbbadc0f3
박준우 <97094633+Joonw00@users.noreply.github.com>
2025-02-04 13:46:58 +0900

    Update user.sql.js

--- srcs/user/user.sql.js
+++ srcs/user/user.sql.js
@@ -2,7 +2,7 @@ export const sql = {
   findUserById: `SELECT user_id, nickname, social_provider, email, name, subscribe, profile_image_url FROM user WHERE user_id = ?`,
   updateNicknameSQL: `UPDATE user SET nickname = ? WHERE user_id = ?`,
   updateStatusSQL: `UPDATE user SET status = ? WHERE user_id = ?`,
-  checkIdOverlap: `SELECT email FROM user WHERE email = ?`,
+  checkIdOverlap: `SELECT email FROM \`user\` WHERE email = ?`,
   postNewUser: `INSERT INTO user (email, password, nickname) VALUES (?, ?,?)`,
   loginGeneralSQL: `SELECT user_id, email FROM user WHERE email = ? AND password = ?;`,
   findUserByEmail: `SELECT user_id, email FROM user WHERE email= ?`,
