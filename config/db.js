1c557587d4c6dc29e76246c6569f162379921fa2
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2025-02-05 11:39:05 +0900

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ export const pool = await mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
-  database: process.env.DB_TABLE,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
