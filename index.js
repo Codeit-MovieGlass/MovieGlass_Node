b869bd825fc08b8f380d58988dc27685d7c2ace3
sinyoung6491 <sinyoung6491@gmail.com>
2025-02-14 19:09:53 +0900

    [fix]cors 추가

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000'}));
 
 
 //health
